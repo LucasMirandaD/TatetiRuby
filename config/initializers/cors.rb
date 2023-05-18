a3c9ff5c51322eae098da7f5b38344028a619f9e
LucasMirandaD <lucasdavidmiranda8@gmail.com>
2023-05-18 15:00:26 -0300

    Correccion de cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,14 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:4201' # Reemplaza esto con la URL de tu front-end
-
-      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
-    end
-    allow do
-        origins 'http://localhost:4200' # Reemplaza esto con la URL de tu front-end
-        
-        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
-      end
+  allow do
+    origins '*' # Permite todas las solicitudes desde cualquier origen (IP)
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
   end
-  
+end
